2f99355d995438222af9a29373a9f4a5fdaa7bd0
Phntme <phntmecode@gmail.com>
2026-02-02 21:54:08 +0700

    nambahin fitur pilih jenis paket dengan menggunakan custom dropdown

--- project/script.js
+++ project/script.js
@@ -1,16 +1,45 @@
-const newOrder = document.getElementById("new-order");
-const closeBtn = document.getElementById("close");
-const newOrderPage = document.getElementById("new-order-page");
+const newOrder = document.getElementById("new-order"),
+  closeBtn = document.getElementById("close"),
+  newOrderPage = document.getElementById("new-order-page"),
+  package = document.getElementById("package-choice"),
+  dropBtn = document.getElementById("drop-btn"),
+  dropIcon = document.getElementById("drop-icon"),
+  dropList = document.getElementById("drop-list"),
+  dropItem = document.querySelectorAll(".drop-item");
 
 newOrder.addEventListener("click", () => {
-  console.log("hello");
   newOrderPage.classList.add("opacity-100");
   newOrderPage.classList.remove("pointer-events-none");
   newOrderPage.classList.add("pointer-events-auto");
+  newOrder.classList.add("opacity-0");
 
   closeBtn.classList.add("opacity-100");
   closeBtn.classList.remove("pointer-events-none");
   closeBtn.classList.add("pointer-events-auto");
 });
 
-closeBtn.addEventListener("click", () => {});
+closeBtn.addEventListener("click", () => {
+  newOrderPage.classList.remove("opacity-100");
+  newOrderPage.classList.add("pointer-events-none");
+  newOrderPage.classList.remove("pointer-events-auto");
+
+  closeBtn.classList.remove("opacity-100");
+  closeBtn.classList.add("pointer-events-none");
+  closeBtn.classList.remove("pointer-events-auto");
+  newOrder.classList.remove("opacity-0");
+});
+
+newOrder.click();
+
+dropBtn.addEventListener("click", () => {
+  dropIcon.classList.toggle("rotate-180");
+  dropList.classList.toggle("opacity-0");
+  dropList.classList.toggle("translate-y-0");
+  dropList.classList.toggle("pointer-events-none");
+});
+
+dropItem.forEach((item) => {
+  item.addEventListener("click", () => {
+    package.textContent = item.textContent;
+  });
+});
